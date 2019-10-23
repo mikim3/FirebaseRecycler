ad2a5d832c2d809a24cdb93b88d3abd72c44934b
Admin <minsu980107@naver.com>
2019-10-23 19:11:57 +0900

    네비게이션에 메뉴누르면 액티비티 전환 구현

--- app/src/main/java/com/example/plusnoticeboard/MainActivity.java
+++ app/src/main/java/com/example/plusnoticeboard/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.plusnoticeboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -125,7 +126,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Intent intent = new Intent(MainActivity.this,NoticeBoard.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
